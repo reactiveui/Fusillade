0ca0fb034b25da944f7ca4f85d2b453881d22272
Paul Betts <paul@paulbetts.org>
2015-04-14 00:07:09 -0700

    Add a simple wrapper for offline support

---
+++ Fusillade/OfflineHttpMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fusillade
+{
+    public class OfflineHttpMessageHandler : HttpMessageHandler
+    {
+        readonly Func<HttpRequestMessage, string, CancellationToken, Task<byte[]>> retrieveBody;
+
+        public OfflineHttpMessageHandler(Func<HttpRequestMessage, string, CancellationToken, Task<byte[]>> retrieveBodyFunc)
+        {
+            retrieveBody = retrieveBodyFunc;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var body = await this.retrieveBody(request, RateLimitedHttpMessageHandler.UniqueKeyForRequest(request), cancellationToken);
+            if (body == null) {
+                return new HttpResponseMessage(System.Net.HttpStatusCode.ServiceUnavailable);
+            }
+
+            var byteContent = new ByteArrayContent(body);
+            return new HttpResponseMessage(HttpStatusCode.OK) { Content = byteContent };
+        }
+    }
+}

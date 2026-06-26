bcc9f037f87bfb62fa2c7d36cc7d2bc898e0842d
Chris Pulman <chris.pulman@yahoo.com>
2026-06-26 23:53:45 +0100

    Update headers and centralize Punchclock usings
    Switches file headers to the new "ReactiveUI and Contributors" copyright/licensing text and updates .editorconfig to enforce it. It also adds Punchclock/Punchclock.Reactive as global usings in the project files, then removes now-unnecessary conditional using blocks from FusilladeSplatBuilderExtensions, NetCache, and RateLimitedHttpMessageHandler, plus a couple of unused usings in a polyfill.

--- src/Fusillade/Polyfills/CallerArgumentExpressionAttribute.cs
+++ src/Fusillade/Polyfills/CallerArgumentExpressionAttribute.cs
@@ -1,13 +1,11 @@
-// Copyright (c) 2019-2026 ReactiveUI Association Incorporated. All rights reserved.
-// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// Copyright (c) 2016-2026 ReactiveUI and Contributors. All rights reserved.
+// ReactiveUI and Contributors licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
 #if !NET5_0_OR_GREATER
 
 // Polyfill implementation adapted from Simon Cropp's Polyfill library
 // https://github.com/SimonCropp/Polyfill
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 
 namespace System.Runtime.CompilerServices;
 
--- src/Fusillade/RateLimitedHttpMessageHandler.cs
+++ src/Fusillade/RateLimitedHttpMessageHandler.cs
@@ -1,13 +1,8 @@
-// Copyright (c) 2019-2026 ReactiveUI Association Incorporated. All rights reserved.
-// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// Copyright (c) 2016-2026 ReactiveUI and Contributors. All rights reserved.
+// ReactiveUI and Contributors licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
 using System.Globalization;
-#if REACTIVE_SHIM
-using Punchclock.Reactive;
-#else
-using Punchclock;
-#endif
 
 #if REACTIVE_SHIM
 namespace Fusillade.Reactive;
